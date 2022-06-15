927653a85e655584b9183f840cee8186123c80ea
Jonathan Esguerra <EsguerraDesign@gmail.com>
2022-06-16 03:38:36 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal Portfolio: Jonathan
+
+Jonathan's personal portfolio of projects made throughout his own web development journey
---
+++ src/scripts.js
@@ -0,0 +1,44 @@
+function scrollTrigger(selector, options = {}) {
+    let els = document.querySelectorAll(selector);
+    els = Array.from(els);
+    els.forEach(el => {
+      addObserver(el, options);
+    });
+}
+
+function addObserver(el, options){
+    if(!('IntersectionObserver' in window)) {
+        if (options.cb) {
+          options.cb(el);
+        }
+        
+        else {
+            entry.target.classList.add('active');
+        }
+        return;
+    }
+
+    let observer = new IntersectionObserver((entries, observer) => { 
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                if (options.cb) {
+                    options.cb(el);
+                }
+                else {
+                    entry.target.classList.add('active');
+                }
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+    observer.observe(el);
+}
+
+scrollTrigger('.project-tile',{
+    rootMargin: '-200px',
+});
+
+
+scrollTrigger('.project-header',{
+    rootMargin: '-200px',
+});
